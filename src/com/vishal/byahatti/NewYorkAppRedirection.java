74323022ddf8926cdceda30965653e0cfa635bd2
vishalgb1 <vishal.byahatti@gmail.com>
2018-09-09 13:44:27 +0530

    first commit

--- src/com/vishal/byahatti/NewYorkAppRedirection.java
+++ src/com/vishal/byahatti/NewYorkAppRedirection.java
@@ -11,44 +11,37 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class NewYorkAppRedirection extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private static final String StringBuffer = null;       
+	private static final String StringBuffer = null;
 	private String defaultSite = new String("https://www.nytimes.com/section/");
-    
-    private void RedirectingLogic(HttpServletRequest request, HttpServletResponse response) {
+
+	private void RedirectingLogic(HttpServletRequest request, HttpServletResponse response) {
 		// TODO Auto-generated method stub
-    	String name = null;
-    	String site = new String("https://www.nytimes.com/section/");
-    	if(request.getParameter("value")!=null && request.getParameter("value").length() > 0)
-    	{
-    		name=request.getParameter("value");
-   			site = site.concat(name);
-   			//System.out.println(site);
-    		response.setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
-    		response.setHeader("Location", site);
-    		
-    	}
-        
-    }
-    public String getSiteName(){
-    	return defaultSite;
-    }
+		String name = null;
+		String site = new String("https://www.nytimes.com/section/");
+		if (request.getParameter("value") != null && request.getParameter("value").length() > 0) {
+			name = request.getParameter("value");
+			site = site.concat(name);
+			// System.out.println(site);
+			response.setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
+			response.setHeader("Location", site);
 
-    
+		}
 
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	public String getSiteName() {
+		return defaultSite;
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		RedirectingLogic(request, response);
-		}	
+	}
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-	
-		
-		
-	
-
 }
--- src/com/vishal/byahatti/NewYorkAppRedirectionTest.java
+++ src/com/vishal/byahatti/NewYorkAppRedirectionTest.java
@@ -8,46 +8,49 @@ import javax.servlet.http.HttpServletResponse;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+public class NewYorkAppRedirectionTest extends Mockito {
 
-public class NewYorkAppRedirectionTest extends Mockito{
+	HttpServletRequest request = mock(HttpServletRequest.class);
+	HttpServletResponse response = mock(HttpServletResponse.class);
+	NewYorkAppRedirection newYorkAppRedirection = new NewYorkAppRedirection();
 
-    HttpServletRequest request = mock(HttpServletRequest.class);       
-    HttpServletResponse response = mock(HttpServletResponse.class);    
-    NewYorkAppRedirection newYorkAppRedirection = new NewYorkAppRedirection();
+	@Test
+	public void worldTest() throws Exception {
+		newYorkAppRedirection.doGet(request, response);
+		when(request.getParameter("value")).thenReturn("world");
+		assertEquals("https://www.nytimes.com/section/world",
+				newYorkAppRedirection.getSiteName() + request.getParameter("value"));
+	}
 
-	
-    @Test
-    public void worldTest() throws Exception {
-        newYorkAppRedirection.doGet(request, response);
-        when(request.getParameter("value")).thenReturn("world");
-        assertEquals("https://www.nytimes.com/section/world", newYorkAppRedirection.getSiteName() + request.getParameter("value"));
-    }
-    
-    @Test
-    public void politicsTest() throws Exception {
-        newYorkAppRedirection.doGet(request, response);
-        when(request.getParameter("value")).thenReturn("politics");
-        assertEquals("https://www.nytimes.com/section/politics",  newYorkAppRedirection.getSiteName() + request.getParameter("value"));
-    }
-    
-    @Test
-    public void businessTest() throws Exception {
-        newYorkAppRedirection.doGet(request, response);
-        when(request.getParameter("value")).thenReturn("business");
-        assertEquals("https://www.nytimes.com/section/business",  newYorkAppRedirection.getSiteName() + request.getParameter("value"));
-    }
-    
-    @Test
-    public void scienceTest() throws Exception {
-        newYorkAppRedirection.doGet(request, response);
-        when(request.getParameter("value")).thenReturn("science");
-        assertEquals("https://www.nytimes.com/section/science",  newYorkAppRedirection.getSiteName() + request.getParameter("value"));
-    }
-    
-    @Test
-    public void sportsTest() throws Exception {
-        newYorkAppRedirection.doGet(request, response);
-        when(request.getParameter("value")).thenReturn("sports");
-        assertEquals("https://www.nytimes.com/section/sports",  newYorkAppRedirection.getSiteName() + request.getParameter("value"));
-    }
+	@Test
+	public void politicsTest() throws Exception {
+		newYorkAppRedirection.doGet(request, response);
+		when(request.getParameter("value")).thenReturn("politics");
+		assertEquals("https://www.nytimes.com/section/politics",
+				newYorkAppRedirection.getSiteName() + request.getParameter("value"));
+	}
+
+	@Test
+	public void businessTest() throws Exception {
+		newYorkAppRedirection.doGet(request, response);
+		when(request.getParameter("value")).thenReturn("business");
+		assertEquals("https://www.nytimes.com/section/business",
+				newYorkAppRedirection.getSiteName() + request.getParameter("value"));
+	}
+
+	@Test
+	public void scienceTest() throws Exception {
+		newYorkAppRedirection.doGet(request, response);
+		when(request.getParameter("value")).thenReturn("science");
+		assertEquals("https://www.nytimes.com/section/science",
+				newYorkAppRedirection.getSiteName() + request.getParameter("value"));
+	}
+
+	@Test
+	public void sportsTest() throws Exception {
+		newYorkAppRedirection.doGet(request, response);
+		when(request.getParameter("value")).thenReturn("sports");
+		assertEquals("https://www.nytimes.com/section/sports",
+				newYorkAppRedirection.getSiteName() + request.getParameter("value"));
+	}
 }
